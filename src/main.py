d33877aa0fd21ee8bcfb04e67c6c22c998eb6ecf
AngusBlance <contact.angusgb@gmail.com>
2023-11-14 13:13:19 +0000

    added data txt's, created read file func,

---
+++ src/main.py
@@ -0,0 +1,19 @@
+text = "this is a test"
+
+def read_file_lines(file):
+    file = open(file, 'r')
+    return file.readlines()
+
+
+def create_dict_from_file(file):
+
+
+def letter_vals(text):
+    main_values = []
+    for i in text:
+        sub_vals = []
+        
+
+
+
+n = readfile("data/values.txt")
