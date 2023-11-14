c4432c818b7c872f894b687f370fa07b94ae0db5
AngusBlance <contact.angusgb@gmail.com>
2023-11-14 14:27:00 +0000

    made function. Gives every letter in txt file scor

--- src/main.py
+++ src/main.py
@@ -1,12 +1,17 @@
-text = "this is a test"
+values = "data/values.txt"
+trees = "data/trees.txt"
+
+
 
 def read_file_lines(file_path):
     
     #open file
     with open(file_path, 'r') as file:
+
+        
         #iterate through the lines in our file and remove and \n's
-    
-        words = [line.rstrip('\n') for line in file]
+
+        words = [line.replace('\n','').upper() for line in file]
 
     return words
 
@@ -30,20 +35,37 @@ def create_dict_from_file(file_path):
     
     return my_dict
 
-def scores_for_words(scores, text):
+def scores_for_words(values, text):
+    scores = []
     for words in text:
+        sub_score = []
+        prev_index = None
         for letter in words:
-            print(letter)
+            if letter == ' ':
+                sub_score.append('$')
+            elif prev_index is not None and prev_index == ' ':
+                sub_score.append(0)
+            else:
+                sub_score.append(values.get(letter))
+            prev_index = letter
+            
+        scores.append(sub_score)
+    
+    return(scores)
         
    
 
+def main(values,trees):
+
+    text = read_file_lines(trees)
+    #print(text)
+    values =  create_dict_from_file(values)
+    #print(values)
+    word_values = scores_for_words(values, text)     
+    print(word_values)
+
+
+main(values, trees)
 
 
 
-
-        
-
-# x = create_dict_from_file("data/values.txt")
-# print(x)
-# y = read_file_lines("data/trees.txt")
-# print(y)
