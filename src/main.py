44fe8e166751b9081bfc725d28ffd6376d7dfaa6
AngusBlance <contact.angusgb@gmail.com>
2023-11-14 13:18:06 +0000

    function turns values into dictionary

--- src/main.py
+++ src/main.py
@@ -5,7 +5,23 @@ def read_file_lines(file):
     return file.readlines()
 
 
-def create_dict_from_file(file):
+def create_dict_from_file(file_path):
+    my_dict = {}
+    # Open the file and read its content
+    with open(file_path, 'r') as file:
+        # Iterate over each line in the file
+        for line in file:
+            # Split the line into key and value (assuming space-separated)
+            key, value = line.strip().split()
+            
+            # Convert the value to an integer
+            value = int(value)
+            
+            # Add the key-value pair to the dictionary
+            my_dict[key] = value
+    
+    return my_dict
+
 
 
 def letter_vals(text):
@@ -14,6 +30,6 @@ def letter_vals(text):
         sub_vals = []
         
 
+x = create_dict_from_file("data/values.txt")
+print(x)
 
-
-n = readfile("data/values.txt")
